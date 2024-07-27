6d2a9c6fa92961a4f6e10dbb6b504ff2f4298cc1
mokgosi <mokgosi@gmail.com>
2024-07-27 11:09:12 +0200

    Initial project setup

---
+++ src/layouts/PageHeader.tsx
@@ -0,0 +1,10 @@
+export function PageHeader() {
+    return <div className="flex gap-10 lg:gap-20 justify-between">
+        <div className="flex gap-4 items-center flex-shrink-0">
+            <button></button>
+            <a href="/">
+                <img src="{logo}" />
+            </a>
+        </div>
+    </div>
+}
